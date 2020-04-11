4ae71b87e4299c2c5aed4d2fc685f98c255a017b
Estefania Saucedo <49458621+teffsaucedo@users.noreply.github.com>
2020-04-10 22:16:40 -0500

    Add files via upload

---
+++ trexchrome.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+Servo myservo;
+
+  
+  
+
+void setup ()
+{
+  myservo.attach(10);
+
+Serial.begin(9600);
+}
+void loop()
+{
+int sensorvalue=analogRead(A0);
+if(sensorvalue>700){
+	myservo.write(41 );}
+else if(sensorvalue<670){
+	myservo.write(53 );
+	delay(200  );
+
+}
+
+
+Serial.println(sensorvalue);
+delay(1);                               
+}
